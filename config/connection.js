557b975705f49c7e42b3660587bb2ebd5d04f651
toomusa <3895499+toomusa@users.noreply.github.com>
2019-08-14 15:05:54 -0700

    connect to jawsdb on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.SQLPASS,
-    database: "burgers_db"
+  host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "zdqrc0judbp33866",
+  password: "h4qudtk2s1d3gbo6",
+  database: "sy3pgm26675ss6c4"
 })
+
+// let connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: process.env.SQLPASS,
+//     database: "burgers_db"
+// })
 
 module.exports = connection;
