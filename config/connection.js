f32a4a7fa3e18907941cead3eff08151d1e5a06d
toomusa <3895499+toomusa@users.noreply.github.com>
2019-08-14 15:33:07 -0700

    clean-up

--- config/connection.js
+++ config/connection.js
@@ -13,12 +13,4 @@ if (process.env.JAWSDB_URL) {
   })
 }
 
-// let connection = mysql.createConnection({
-//   host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   port: 3306,
-//   user: "zdqrc0judbp33866",
-//   password: "h4qudtk2s1d3gbo6",
-//   database: "sy3pgm26675ss6c4"
-// })
-
 module.exports = connection;
