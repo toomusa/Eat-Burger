ecfea8db969c786afecc164a307e888fce9fbb2a
toomusa <3895499+toomusa@users.noreply.github.com>
2019-08-14 15:18:49 -0700

    link to environment variable

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-  host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "zdqrc0judbp33866",
-  password: "h4qudtk2s1d3gbo6",
-  database: "sy3pgm26675ss6c4"
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.SQLPASS,
+    database: "burgers_db"
+  })
+}
 
 // let connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: process.env.SQLPASS,
-//     database: "burgers_db"
+//   host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "zdqrc0judbp33866",
+//   password: "h4qudtk2s1d3gbo6",
+//   database: "sy3pgm26675ss6c4"
 // })
 
 module.exports = connection;
